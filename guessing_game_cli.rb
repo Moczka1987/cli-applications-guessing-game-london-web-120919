a1f92643b345ad738d6fc8d325dafceb5fd247cf
Mariola Pasko <paskomariola@gmail.com>
2019-12-05 22:07:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,4 +4,14 @@ computer_number = rand(6) + 1
 end 
 
 def user_prompt
-  puts ""
+  puts "Pick a number between 1 and 6."
+  user_number = gets.chomp 
+  if computer_number == user_number
+    puts "You guessed the correct number"
+  elsif 
+    puts "Sorry! The computer guessed #{computer_number}."
+  else user_number = exit
+    puts "Goodbye"
+end
+
+
